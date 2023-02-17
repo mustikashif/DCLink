c36e630f972347bbb9bb3920a3ef473185b68c14
mustikashif <muskash2005@gmail.com>
2023-02-17 18:42:31 +0400

    setting up database

--- Website/__init__.py
+++ Website/__init__.py
@@ -3,15 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'mustafa2005'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User
+    
+    with app.app_context():
+        db.create_all()
     
 
     return app 
---
+++ Website/models.py
@@ -0,0 +1,9 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True) 
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    date = db.Column(db.Date(timezone=True), default = func.now())
