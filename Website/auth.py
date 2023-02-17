f332b296131c6640214fb6f600cbe69cacfde8d4
mustikashif <muskash2005@gmail.com>
2023-02-17 19:07:44 +0400

    creating new users

--- Website/auth.py
+++ Website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, flash
+from . import db
+from .models import User
+from flask_login import login_user, logout_user, login_required
 
 auth = Blueprint("auth", __name__)
 
@@ -10,9 +13,27 @@ def login():
 
 @auth.route("/signup", methods = ['GET','POST'])
 def sign_up():
-    email = request.form.get("email")
-    password1 = request.form.get("password1")
-    password2 = request.form.get("password2")
+    if request.method == "POST":
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        email_exists = User.query.filter_by(email=email).first()
+        if email_exists:
+            flash('Email is already in use', category='error')
+        elif password1 != password2:
+            flash("Passwords do not match", category='error')
+        elif len(password1) < 8:
+            flash("Password must be at least 8 characters long", category='error')
+        else:
+            new_user = User(email = email, password = password1)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("User Created!")
+            return redirect(url_for('views.home'))
+
+
+    
     return render_template("signup.html")
 
 @auth.route("/logout")
